8a2bde605a12e638acc66db6d7b7a8df98a2d59c
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-01-30 21:35:27 +0300

    final

--- src/Loader.java
+++ src/Loader.java
@@ -5,9 +5,10 @@ public class Loader
         for (int i=0; i<=512; i++) {
             System.out.println("Символ с номером: "+ i +" - " + (char)i);
         }
-        System.out.println((int) 'а');
-        System.out.println((int) 'я');
         System.out.println((int) 'А');
         System.out.println((int) 'Я');
+        System.out.println((int) 'а');
+        System.out.println((int) 'я');
+
     }
 }
